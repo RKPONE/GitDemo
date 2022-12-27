15cab6c1f77867ade092dce2f22510f1a90cfb3e
RKDigital <RKDigital@DESKTOP-269OAN9>
2022-12-27 19:45:15 +0530

    Base Project

---
+++ src/com/Training/com/Sample.java
@@ -0,0 +1,12 @@
+package com.Training.com;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is Git Demo");
+
+	}
+
+}
